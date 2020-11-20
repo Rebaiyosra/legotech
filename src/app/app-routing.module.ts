e80a987d81003c44648db27a2fccf57fe91742dd
ahlem24 <ahlemmsalmii2@gmail.com>
2020-11-20 22:24:50 +0100

    :)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { RegisterUtilisateurComponent} from './register-utilisateur/register-utilisateur.component';
+import { RegisterResponsableComponent} from './register-responsable/register-responsable.component';
+import { SignInUtilisateurComponent} from './sign-in-utilisateur/sign-in-utilisateur.component';
+import { SignInResComponent} from './sign-in-res/sign-in-res.component';
+const routes: Routes = [
+  {path:'register-utilisateur', component:RegisterUtilisateurComponent},
+  {path:'register-responsable', component:RegisterResponsableComponent},
+  {path:'sign-utilisateur', component:SignInUtilisateurComponent},
+  {path:'sign-res', component:SignInResComponent},
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
